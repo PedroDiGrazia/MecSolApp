d7c043f2a22391cf89786eea460a49c297378ba7
Di Grazia <105602165+PedroDiGrazia@users.noreply.github.com>
2023-05-22 13:03:05 -0300

    Update Aplicativo.py

--- Aplicativo.py
+++ Aplicativo.py
@@ -22,13 +22,17 @@ j=0
 for i in range(0, QtdApoios):
     n = n + 1
     vetor[m] = int(input(f"Digite o numero do Apoio no ponto {n} (1,2,3): "))
+    if vetor[m] <1 and vetor[m] >3: 
+                print()
+                print("NUMERO INVALIDO! DIGITE NOVAMENTE")
+                print()
     print(QtdApoios)
     for j in range(0, QtdApoios):
-        if vetor[m] <1 and vetor[m] >3: 
-            print()
-            print("NUMERO INVALIDO! DIGITE NOVAMENTE")
-            print()
             vetor[m] = int(input(f"Digite o numero do Apoio no ponto {n} (1,2,3): "))
+            if vetor[m] <1 and vetor[m] >3: 
+                print()
+                print("NUMERO INVALIDO! DIGITE NOVAMENTE")
+                print()
             m = m + 1
     m=m+1
     print(vetor)
@@ -63,7 +67,7 @@ vetor2 = list(range(QtdApoios))
 print("Origem = 0m")
 for i in range(0, QtdApoios):
     n = n+1
-    vetor2[j] = int(input(f"Qual é a distância em relação à origem do apoio {n}: "))
+    vetor2[j] = float(input(f"Qual é a distância em relação à origem do apoio {n}: "))
     j = j+1
 print()
 
@@ -72,6 +76,13 @@ TamanhoBarra =  vetor2[-1] - vetor2[0]
 print(f"O tamanho da barra é de: {TamanhoBarra}m")
 
 QtdForcas= int(input("Quantas forças serão aplicadas: "))
+
+print()
+print("!!!Considere as Forças como:!!!")
+print("-> Distribuida = 1")
+print("-> Concentrada = 2")
+print()
+input("Aperte <Enter> para continuar!")
 
 Forca = list(range(QtdForcas))
 c=0
@@ -85,5 +96,17 @@ for i in range(0,QtdForcas):
         print("Força concentrada")
         Forca[c]= 'Concentrada' 
     c=c+1
-
 print(Forca)
+i=0
+c=0
+for i in range(0,QtdForcas):
+    if Forca[c] == 'Concentrada':
+        print()
+        PosForcaC = float(input("Onde deseja aplicar a força: ")) 
+    else:
+        print()
+        print("Utilize um dos apoios como origem!")
+        PosForcaD = float(input(f"Digite o intervalo da força em relação a origem até {TamanhoBarra}m: "))
+        if PosForcaD > TamanhoBarra:
+           PosForcaD = float(input("Digite Novamente: "))
+    c=c+1
